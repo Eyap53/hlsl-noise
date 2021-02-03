23213479f2fb8990650d65215d4630bb8a2efb95
Mael Lacour <public.dev@maellacour.com>
2021-02-03 16:49:57 +0100

    Adding SimplexNoise3D with gradient

--- src/SimplexNoise3D.hlsl
+++ src/SimplexNoise3D.hlsl
@@ -5,6 +5,7 @@
 //               Copyright (C) 2021 Lacour Mael.
 //               Distributed under the MIT License. See LICENSE file.
 // 
+// TODO: Test the performance of fmod (x, 289.0). May be inferior with the use of pre-computed 1 / 289.0 value.
 
 float4 permute(float4 x)
 {
@@ -21,33 +22,33 @@ float snoise(float3 v)
     const float2 C = float2(0.166666666666666667, 0.333333333333333333); //float2( 1/6, 1/3)
     const float4 D = float4(0.0, 0.5, 1.0, 2.0);
 
-	// First corner
+    // First corner
     float3 i = floor(v + dot(v, C.yyy));
     float3 x0 = v - i + dot(i, C.xxx);
 
-	// Other corners
+    // Other corners
     float3 g = step(x0.yzx, x0.xyz);
     float3 l = 1.0 - g;
     float3 i1 = min(g.xyz, l.zxy);
     float3 i2 = max(g.xyz, l.zxy);
 
- 	//   x0 = x0 - 0.0 + 0.0 * C.xxx;
-	//   x1 = x0 - i1  + 1.0 * C.xxx;
- 	//   x2 = x0 - i2  + 2.0 * C.xxx;
-  	//   x3 = x0 - 1.0 + 3.0 * C.xxx;
+    //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+    //   x1 = x0 - i1  + 1.0 * C.xxx;
+    //   x2 = x0 - i2  + 2.0 * C.xxx;
+    //   x3 = x0 - 1.0 + 3.0 * C.xxx;
     float3 x1 = x0 - i1 + C.xxx;
     float3 x2 = x0 - i2 + C.yyy; // 2 * C.x = 1/3 = C.y
     float3 x3 = x0 - D.yyy;      // -1 + 3 * C.x = -0.5 = -D.y
 
-	// Permutations
+    // Permutations
     i = fmod(i, 289.0);
     float4 p = permute(permute(permute(
              i.z + float4(0.0, i1.z, i2.z, 1.0))
            + i.y + float4(0.0, i1.y, i2.y, 1.0))
            + i.x + float4(0.0, i1.x, i2.x, 1.0));
 
-	// Gradients: 7x7 points over a square, mapped onto an octahedron.
-	// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+    // Gradients: 7x7 points over a square, mapped onto an octahedron.
+    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
     float3 ns = 0.142857142857 * D.wyz - D.xzx; // 0.142857142857 = 1/7
 
     float4 j = fmod(p, 49.0);
@@ -62,11 +63,11 @@ float snoise(float3 v)
     float4 b0 = float4(x.xy, y.xy);
     float4 b1 = float4(x.zw, y.zw);
 
-  	//float4 s0 = float4(lessThan(b0,0.0))*2.0 - 1.0;
-  	//float4 s1 = float4(lessThan(b1,0.0))*2.0 - 1.0;
+      //float4 s0 = float4(lessThan(b0,0.0))*2.0 - 1.0;
+      //float4 s1 = float4(lessThan(b1,0.0))*2.0 - 1.0;
     float4 s0 = floor(b0) * 2.0 + 1.0;
     float4 s1 = floor(b1) * 2.0 + 1.0;
-    float4 sh = -step(h, float4(0, 0, 0, 0));
+    float4 sh = -step(h, float4(0.0, 0.0, 0.0, 0.0));
 
     float4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
     float4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
@@ -76,16 +77,98 @@ float snoise(float3 v)
     float3 p2 = float3(a1.xy, h.z);
     float3 p3 = float3(a1.zw, h.w);
 
-	//Normalise gradients
+    //Normalise gradients
     float4 norm = taylorInvSqrt(float4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
     p0 *= norm.x;
     p1 *= norm.y;
     p2 *= norm.z;
     p3 *= norm.w;
 
-	// Mix final noise value
+    // Mix final noise value
     float4 m = max(0.5 - float4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
     m = m * m;
     return 105.0 * dot(m * m, float4(dot(p0, x0), dot(p1, x1),
                                 dot(p2, x2), dot(p3, x3)));
+}
+
+float snoise_grad(float3 v, out float3 gradient)
+{
+    const float2 C = float2(0.166666666666666667, 0.333333333333333333); //float2( 1/6, 1/3)
+    const float4 D = float4(0.0, 0.5, 1.0, 2.0);
+
+    // First corner
+    float3 i = floor(v + dot(v, C.yyy));
+    float3 x0 = v - i + dot(i, C.xxx);
+
+    // Other corners
+    float3 g = step(x0.yzx, x0.xyz);
+    float3 l = 1.0 - g;
+    float3 i1 = min(g.xyz, l.zxy);
+    float3 i2 = max(g.xyz, l.zxy);
+
+    //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+    //   x1 = x0 - i1  + 1.0 * C.xxx;
+    //   x2 = x0 - i2  + 2.0 * C.xxx;
+    //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+    float3 x1 = x0 - i1 + C.xxx;
+    float3 x2 = x0 - i2 + C.yyy; // 2 * C.x = 1/3 = C.y
+    float3 x3 = x0 - D.yyy;      // -1 + 3 * C.x = -0.5 = -D.y
+
+    // Permutations
+    i = fmod(i, 289.0);
+    float4 p = permute(permute(permute(
+             i.z + float4(0.0, i1.z, i2.z, 1.0))
+           + i.y + float4(0.0, i1.y, i2.y, 1.0))
+           + i.x + float4(0.0, i1.x, i2.x, 1.0));
+
+    // Gradients: 7x7 points over a square, mapped onto an octahedron.
+    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+    float3 ns = 0.142857142857 * D.wyz - D.xzx; // 0.142857142857 = 1/7
+
+    float4 j = fmod(p, 49.0);
+
+    float4 x_ = floor(j * 0.142857142857); // j * 1/7
+    float4 y_ = floor(j - 7.0 * x_);
+
+    float4 x = x_ * ns.x + ns.yyyy;
+    float4 y = y_ * ns.x + ns.yyyy;
+    float4 h = 1.0 - abs(x) - abs(y);
+
+    float4 b0 = float4(x.xy, y.xy);
+    float4 b1 = float4(x.zw, y.zw);
+
+      //float4 s0 = float4(lessThan(b0,0.0))*2.0 - 1.0;
+      //float4 s1 = float4(lessThan(b1,0.0))*2.0 - 1.0;
+    float4 s0 = floor(b0) * 2.0 + 1.0;
+    float4 s1 = floor(b1) * 2.0 + 1.0;
+    float4 sh = -step(h, float4(0.0, 0.0, 0.0, 0.0));
+
+    float4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+    float4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+
+    float3 p0 = float3(a0.xy, h.x);
+    float3 p1 = float3(a0.zw, h.y);
+    float3 p2 = float3(a1.xy, h.z);
+    float3 p3 = float3(a1.zw, h.w);
+
+    //Normalise gradients
+    float4 norm = taylorInvSqrt(float4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
+    p0 *= norm.x;
+    p1 *= norm.y;
+    p2 *= norm.z;
+    p3 *= norm.w;
+
+    // Mix final noise value
+    float4 m = max(0.5 - float4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
+    float4 m2 = m * m;
+    float4 m4 = m2 * m2;
+    float4 pdotx = float4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3));
+
+    // Determine noise gradient
+    float4 temp = m2 * m * pdotx;
+    gradient = -8.0 * (temp.x * x0 + temp.y * x1 + temp.z * x2 + temp.w * x3);
+    gradient += m4.x * p0 + m4.y * p1 + m4.z * p2 + m4.w * p3;
+    gradient *= 105.0;
+
+    return 105.0 * dot(m4, pdotx);
 }
